0f51effaef354635bfe93021da7a69e050094a99
nkleinmann <nekleinmann@gmail.com>
2022-04-24 12:09:12 -0400

    add category-item component

--- src/App.js
+++ src/App.js
@@ -1,44 +1,42 @@
+import CategoryItem from './components/category-item/category-item.component';
+
+import './categories.styles.scss'
+
 const App = () =>  {
 
   const categories = [
     {
-      id: 1,
-      title: 'Hats',
-      subtitle: 'Shop Now'
+      "id": 1,
+      "title": "hats",
+      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
     },
     {
-      id: 2,
-      title: 'Jackets',
-      subtitle: 'Shop Now'
+      "id": 2,
+      "title": "jackets",
+      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
     },
     {
-      id: 3,
-      title: 'Sneakers',
-      subtitle: 'Shop Now'
+      "id": 3,
+      "title": "sneakers",
+      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
     },
     {
-      id: 4,
-      title: 'Womens',
-      subtitle: 'Shop Now'
+      "id": 4,
+      "title": "womens",
+      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
     },
     {
-      id: 5,
-      title: 'Mens',
-      subtitle: 'Shop Now'
+      "id": 5,
+      "title": "mens",
+      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
     }
   ]
+  
 
   return (
    <div className="categories-container">
      {categories.map((category) => (
-      <div key={category.id} className="category-container">
-        {/* <img /> */}
-        <div className='background-image' />
-        <div className="category-body-container">
-          <h2>{category.title}</h2>
-          <p>{category.subtitle}</p>
-        </div>
-      </div>
+       <CategoryItem key={category.id} category={category} />
      ))}
    </div>
   );
