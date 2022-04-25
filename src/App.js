59ee1cf0881c3ddfdc25c7a11198358d3bb2bd20
nkleinmann <nekleinmann@gmail.com>
2022-04-25 17:06:14 -0400

    add basic navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import { Routes, Route } from 'react-router-dom'
 
-import Home from "./routes/home/home.component";
+import Navigation from './routes/navigation/navigation.component'
+import Home from "./routes/home/home.component"
+
+const Shop = () => {
+  return <h1>I am the shop page</h1>
+}
 
 const App = () =>  {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path='/' element={<Navigation />}>
+        {/* saying when the / route is matched, what ever route has index will also render */}
+        <Route index element={<Home />} />
+        <Route path='/shop' element={<Shop />} />
+      </Route>
     </Routes>
   );
 }
---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,28 @@
+import { Fragment } from 'react'
+import { Outlet, Link } from 'react-router-dom'
+
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'
+
+import './navigation.styles.scss'
+
+const Navigation = () => {
+    return (
+        // helpful for outer parent
+      <Fragment>
+        <div className='navigation'>
+            <Link className='logo-container'to='/' >
+                <CrwnLogo className='logo' />
+            </Link>
+          <div className='nav-links-container'>
+              <Link className='nav-link' to='/shop'>
+                  SHOP
+              </Link>
+          </div>
+        </div>
+        {/* Outlet allows the elements of page to change for certain routes */}
+        <Outlet />
+      </Fragment>
+    )
+  }
+
+  export default Navigation
