ade2b355118b884b6e310f17887ce275e222ec15
nkleinmann <nekleinmann@gmail.com>
2022-05-05 10:42:14 -0400

    finish authentication page

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -36,6 +36,16 @@ const SignInForm = () => {
             console.log(response)
             resetFormFields()
         } catch(error) {
+            switch(error.code) {
+                case 'auth/wrong-password':
+                    alert('Incorrect password for email')
+                    break
+                case 'auth/user-not-found':
+                    alert('No user associated with this email')
+                    break
+                default: 
+                    console.log(error)
+            }
         }
     }
 
@@ -68,7 +78,7 @@ const SignInForm = () => {
                      />
                 <div className='buttons-container'>
                     <Button type='submit'>Sign In</Button>
-                    <Button buttonType='google' onClick={signInWithGoogle}>Google sign in</Button>
+                    <Button type='button' buttonType='google' onClick={signInWithGoogle}>Google sign in</Button>
                 </div>
             </form>
         </div>
